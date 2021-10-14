2447753b27e6a657bb6c931ce52a0614a6d6df0c
unknown <416212316@qq.com>
2021-10-14 23:03:12 +0800

    index signatures

--- src/demo2.ts
+++ src/demo2.ts
@@ -1,7 +1,16 @@
-type ClassT<T> = new (...ags: any[]) => T
+type ClassT<T> = new (...args: any[]) => T
 
-function createClass<T extends ClassT<T>>(base: T) {
+function createClass<TBase extends ClassT<Object>>(base: TBase) {
     return class extends base {
-
+        name = 'coboy'
     }
 }
+
+class User{
+
+}
+
+const newUser = createClass(User)
+const newUserInstance = new newUser()
+newUserInstance.name
+
