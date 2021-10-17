753d4676efe82254736fe091a856a1994f43db9e
unknown <416212316@qq.com>
2021-10-17 19:14:26 +0800

    函数重载

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 const myName: string = 'coboy'
-document.querySelector("#app")!.innerHTML = myName
+document.querySelector('#app')!.innerHTML = myName
 
-interface List{
-    id: number,
-    name: string,
-    desc?: string
+interface List {
+  id: number
+  name: string
+  desc?: string
 }
 
-interface Result{
-    data: List[]
+interface Result {
+  data: List[]
 }
 
 function filter(result: Result) {
-    result.data.forEach(o => {
-        if(o.desc) {
-            console.log(o.desc)
-        }
-    })
+  result.data.forEach((o) => {
+    if (o.desc) {
+      console.log(o.desc)
+    }
+  })
 }
 
 const result1 = {
-    data: [
-        {id: 1, name: 'name1'},
-        {id: 2, name: 'name2'}
-    ]
+  data: [
+    { id: 1, name: 'name1' },
+    { id: 2, name: 'name2' },
+  ],
 }
 
 filter(result1)
@@ -31,10 +31,10 @@ filter(result1)
 // 实际过程中，可能后端返回的数组会增加一些数据
 
 const result2 = {
-    data: [
-        {id: 1, name: 'name1', desc: "正在学习typescript"},
-        {id: 2, name: 'name2'}
-    ]
+  data: [
+    { id: 1, name: 'name1', desc: '正在学习typescript' },
+    { id: 2, name: 'name2' },
+  ],
 }
 filter(result2)
 
@@ -69,13 +69,48 @@ filter(result2)
 
 // 数字索引
 
-interface StringArray{
-    [index: number]: string
+interface StringArray {
+  [index: number]: string
 }
-const chars: StringArray = [ 'a', 'b' ]
+const chars: StringArray = ['a', 'b']
 
 // 字符串索引
-interface Item{
-    [index: string]: any,
-    [key: number]: number
+interface Item {
+  [index: string]: any
+  [key: number]: number
 }
+
+class A {
+  sum(a: number, b: number) {
+    return a + b
+  }
+}
+
+class B extends A {
+  sub(a: number, b: number) {
+    return a - b
+  }
+}
+
+// type R<T> = T extends A|B ? A : B
+
+// type RR<T> = typeof T extends typeof A ? A : B;
+
+type at = typeof A
+type bt = typeof B
+
+type f<T> = (p?: string) => T
+
+function maker(): B
+function maker(p:string): A
+function maker(p?:string) {
+    if(p) {
+        return new A()
+    }
+    return new B()
+}
+
+
+
+const instane = maker()
+instane.sub(1,2)
