203afe8f406284cd9feaee250cba44bc9f0cf422
unknown <416212316@qq.com>
2021-10-26 22:11:03 +0800

    联合类型

--- src/advanced1.ts
+++ src/advanced1.ts
@@ -13,6 +13,14 @@ let pet: DogInterface & CatInterface = {
 }
 // 需要注意的是交叉类型看名称给人的感觉是几个类型的交集，实际上是取所有类型的并集。
 
+// 联合类型 
+// 所谓联合类型就是指声明的类型并不确定，可以为多个类型中的一个。
+let ameType: number | string = '1' // 可以等于数字也可以等于字符串
+
+// 字面量类型
+// 有的时候我们不仅需要限定一个变量的类型，而且要限定变量的取值在某一个特定的范围内
+let b: 'a' | 'b' | 'c'
+
 class DogImpl implements DogInterface {
     run() {}
     eat() {}
