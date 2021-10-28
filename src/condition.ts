9e3470beacd02d8e74045305d0d64bf73f28e79d
unknown <416212316@qq.com>
2021-10-28 21:18:41 +0800

    条件类型

--- src/condition.ts
+++ src/condition.ts
@@ -1,6 +1,7 @@
 // 条件类型
 // 条件类型是一种由条件表达式决定的类型
 // T extends U ? X : Y
+// 意思是如果类型T可以赋值给类型U，那么结果类型就是X类型，否则就是Y类型
 type TypeName<T> = 
     T extends string ? "string" :
     T extends number ? 'number' :
